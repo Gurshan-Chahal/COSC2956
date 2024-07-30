233fde1eb55a07a167e4d06a83a4f36f6f82e70f
Gurshan-Chahal <gurshanchahal0001xx@gmail.com>
2024-07-30 16:06:04 -0400

    Delete Gurshandeep Final Exam directory

--- Gurshandeep Final Exam/algomau.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$title = 'index';
-require_once './includes/header.php';
-?>
-<div class="row flex-fill"><iframe src="https://algomau.ca/academics/school-computer-science-technology/"></iframe></div>
-    <?php
-    require_once './includes/footer.php';
-    ?>
